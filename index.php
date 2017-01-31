76c78816c7a79b08161915c85395636aef5ac1bc
Student Ambassador <studentambassador@champlain.edu>
2017-01-31 16:09:08 -0500

    Initial Upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Project Tracker</title>
+    <link rel="stylesheet" href="./css/styles.css">
+  </head>
+  <body>
+    <nav>
+      <a id="logo-wrapper" href="index.html">
+          <div class="logo">
+            <span>STU</span>
+            <img src="./img/shield_color.png" alt=""/>
+            <span>ENT &nbsp;</span>
+          </div>
+          <div class="logo">
+            <span>AMBASSADORS</span>
+            <span>Project Tracker</span>
+          </div>
+      </a>
+      <div class="pages">
+        <a href="index.html"><li>Home</li></a>
+        <a href="./pages/extensions.html"><li>Extensions</li></a>
+        <a href="./pages/counselormap.html"><li>Counselor Map</li></a>
+      </div>
+    </nav>
+    <div class="main-content">
+      <h1>Current Projects</h1>
+      <div class="project">
+        <span class="project-date">Fri, Jan 20</span>
+        <span class="project-requester">Veronica Lewis</span>
+        <span class="project-title">Read the Factbook</span>
+        <span class="project-status">In Progress</span>
+        <span class="project-content">We recently discovered an SA page under student leadership opportunities (http://www.champlain.edu/current-students/student-life-and-activities/clubs-and-organizations/student-leadership-opportunities/admissions-ambassador). Drawing inspiration from the other student leadership pages listed there (OL, Eco-rep, PA, etc), work together to craft info on who you are, what you do, and how you do it. Generally mention other campus involvements and the diversity in majors, but really focus on making the page convey who you are. By the due date, Jovan and Veronica will review before getting it published. Contact either of us with any questions.</span>
+        <span class="project-initials">MF, JY, JM</span>
+
+        <div class="project-buttons">
+          <button type="button" name="add">Add Initials</button>
+          <button type="button" name="approval" id="approval">Submit for Approval</button>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.2.0/js/mdb.min.js" charset="utf-8"></script>
+    <script type="text/javascript">
+      var project = {
+        "date": "1/31/2017",
+        "description": "This is a description",
+        "initials": "MF, DV, CT",
+        "name": "Mega Walk 2.0",
+        "notes": "Not done",
+        "owner": "Veronica Lewis",
+        "status": 0
+      }
+      $("#approval").click(function(){
+        console.log("Click");
+        $.ajax({
+  				url: "https://api.mongolab.com/api/1/databases/projects/collections/projects?apiKey=61N67c8_8YDZElxIRJADXPbmhp2PjD0t",
+  				type: "POST",
+  				data: JSON.stringify( project ),
+  				contentType: "text/plain",
+          xhrFields: {
+            withCredentials: false
+          }
+  			}).done(function( msg ) {
+  				console.log(msg);
+  			});
+      });
+    </script>
+  </body>
+</html>
